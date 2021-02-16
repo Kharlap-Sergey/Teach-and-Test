369f575534e4ea7a3cd2eb18d5e829fdfcc85f90
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-02-16 19:51:57 +0300

    Add AuthService

---
+++ TeachAndTest-server/TeachAndTest.BusinessLogic/Auth/AuthenticateService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using TeachAndTest.Models.Entities;
+
+namespace TeachAndTest.BusinessLogic.Auth
+{
+    [ServiceImplementation(typeof(IAuthenticateService))]
+    public class AuthenticateService : IAuthenticateService
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AuthenticateService(
+                UserManager<User> userManager
+                , SignInManager<User> signInManager
+            )
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public async Task<User> LoginAsync(
+            string userName
+            , string password
+            , bool isPersistent = true
+            , bool isShouldBeLocked = false
+            )
+        {
+            var result =
+                await signInManager.PasswordSignInAsync(
+                    userName
+                    , password
+                    , isPersistent
+                    , isShouldBeLocked
+                    );
+
+            if( !result.Succeeded )
+            {
+                //todo Implement this feature 
+                //throw new InvalidCredentialException("The user name or password is not correct");
+            }
+
+            User user = await userManager.FindByNameAsync(userName);
+            return user;
+        }
+    }
+}
---
+++ TeachAndTest-server/TeachAndTest.BusinessLogic/Auth/IAuthenticateService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using TeachAndTest.Models.Entities;
+
+namespace TeachAndTest.BusinessLogic.Auth
+{
+    public interface IAuthenticateService
+    {
+        public Task<User> LoginAsync(
+            string userName
+            , string password
+            , bool isPersistent = true
+            , bool isShouldBeLocked = false
+            );
+    }
+}
