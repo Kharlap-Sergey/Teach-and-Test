0b5b70defa3415c448c63456f97be2e83af66ca8
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-02-16 20:00:20 +0300

    Add Account service and creation user method

---
+++ TeachAndTest-server/TeachAndTest.BusinessLogic/Account/AccountService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using TeachAndTest.Models.Entities;
+
+namespace TeachAndTest.BusinessLogic.Account
+{
+    [ServiceImplementation(typeof(IAccountService))]
+    public class AccountService : IAccountService
+    {
+        private readonly UserManager<User> userManager;
+
+        public AccountService(
+            UserManager<User> userManager
+            )
+        {
+            this.userManager = userManager;
+        }
+        public async Task<User> CreateAsync(User user, string password)
+        {
+            var result = await userManager.CreateAsync(user, password);
+            if( !result.Succeeded )
+            {
+                //todo implement logic for exception here
+                throw new System.Exception();
+            }
+            return await userManager.FindByNameAsync(user.UserName);
+        }
+    }
+}
---
+++ TeachAndTest-server/TeachAndTest.BusinessLogic/Account/IAccountService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TeachAndTest.Models.Entities;
+
+namespace TeachAndTest.BusinessLogic.Account
+{
+    public interface IAccountService
+    {
+        public Task<User> CreateAsync(User user, string password);
+    }
+}
