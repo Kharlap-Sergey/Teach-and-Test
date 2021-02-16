6d21670685213fe0be3386ec453055e624b13c96
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-02-16 12:47:59 +0300

    Add db context class

---
+++ TeachAndTest-server/TeachAndTest.Domain/CustomDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TeachAndTest.Models.Entities;
+
+namespace TeachAndTest.Domain
+{
+    public class CustomDbContext : IdentityDbContext<User, Role, int>
+    {
+        public CustomDbContext(
+            DbContextOptions<CustomDbContext> options
+            )
+            : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
