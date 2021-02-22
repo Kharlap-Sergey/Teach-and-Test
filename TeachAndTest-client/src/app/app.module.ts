e293c4ec03b464735a47bef3df7a2836424f63d5
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-02-22 10:52:06 +0300

    Add custom input component

--- TeachAndTest-client/src/app/app.module.ts
+++ TeachAndTest-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomInputComponent } from './shared/components/custom-input/custom-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomInputComponent
   ],
   imports: [
     BrowserModule,
