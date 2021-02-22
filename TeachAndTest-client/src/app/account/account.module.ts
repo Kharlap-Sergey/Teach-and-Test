185ce67ea38b16cd4873ad29e53e347eea319362
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-02-22 12:19:06 +0300

    Fix custom input problem

--- TeachAndTest-client/src/app/account/account.module.ts
+++ TeachAndTest-client/src/app/account/account.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
+import { SharedModule } from './../shared/shared.module';
 
 
 @NgModule({
   declarations: [RegistrationComponent, LoginComponent],
   imports: [
-    CommonModule,
-    AccountRoutingModule
+  CommonModule,
+    AccountRoutingModule,
+    SharedModule
   ]
 })
 export class AccountModule { }
