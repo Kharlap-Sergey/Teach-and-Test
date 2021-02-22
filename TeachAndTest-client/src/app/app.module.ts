3c8b9d6f706a438e953cf8f1cebcc4e5a35066eb
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-02-22 11:01:45 +0300

    Add custom input

--- TeachAndTest-client/src/app/app.module.ts
+++ TeachAndTest-client/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomInputComponent } from './shared/components/custom-input/custom-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomInputComponent
   ],
   imports: [
     BrowserModule,
---
+++ TeachAndTest-client/src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomInputComponent } from './components/custom-input/custom-input.component';
+
+@NgModule({
+  declarations: [CustomInputComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
