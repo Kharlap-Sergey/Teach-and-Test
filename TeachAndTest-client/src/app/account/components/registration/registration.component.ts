b220d1f19564d56e8bb16bfedea18dd8f18e3dc6
snovis <sergeygoodgood@gmail.com>
2021-02-26 23:16:41 +0300

    Add registration form

--- TeachAndTest-client/src/app/account/components/registration/registration.component.ts
+++ TeachAndTest-client/src/app/account/components/registration/registration.component.ts
@@ -1,22 +1,51 @@
-import { Component, OnInit, Self } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  Self,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+} from '@angular/forms';
 import { AccountService } from '../../services/account.service';
 
 @Component({
   selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss'],
+  templateUrl:
+    './registration.component.html',
+  styleUrls: [
+    './registration.component.scss',
+  ],
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent
+  implements OnInit {
+  email = new FormControl();
+  firstname = new FormControl();
+  lastname = new FormControl();
+  password = new FormControl();
+  passwordRepined = new FormControl();
+
+  registrationForm = new FormGroup({
+    email: this.email,
+    firstname: this.firstname,
+    lastname: this.lastname,
+    password: this.password,
+  });
+
   info: any;
-  constructor(private remoteService: AccountService) {}
+  constructor(
+    private remoteService: AccountService
+  ) {}
 
   ngOnInit(): void {}
 
   onSubmit(value: any): void {
-    //this.remoteService.test().subscribe((data: any) => this.info = data );
     this.remoteService
       .registrateNewUser(value)
-      .subscribe((data: any) => {this.info = JSON.stringify(data)});
+      .subscribe((data: any) => {
+        this.info = JSON.stringify(
+          data
+        );
+      });
   }
 }
