55e415553b5524f38afca445c825c78f8d5bff61
snovis <sergeygoodgood@gmail.com>
2021-03-07 13:45:41 +0300

    Add frame for password input

---
+++ TeachAndTest-client/src/app/shared/components/inputs/password-input/password-input.component.ts
@@ -0,0 +1,21 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Self,
+} from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { ReactiveInputComponent } from '@shared/components/inputs/reactive-input/reactive-input.component';
+
+@Component({
+  selector: 'app-password-input',
+  templateUrl: './password-input.component.html',
+  styleUrls: ['./password-input.component.scss'],
+})
+export class PasswordInputComponent {
+  constructor(public controlDir: NgControl) {
+    //super(controlDir);
+  }
+
+  ngOnInit(): void {}
+}
--- TeachAndTest-client/src/app/shared/shared.module.ts
+++ TeachAndTest-client/src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { GenericInputComponent } from './components/generic-input/generic-input.component';
 import { CounterInputComponent } from './components/counter-input/counter-input.component';
-import { ReactiveInputComponent } from "./components/reactive-input/reactive-input.component";
+import { ReactiveInputComponent } from "./components/inputs/reactive-input/reactive-input.component";
+import { PasswordInputComponent } from './components/inputs/password-input/password-input.component';
 
 @NgModule({
-  declarations: [GenericInputComponent,CounterInputComponent, ReactiveInputComponent],
+  declarations: [GenericInputComponent,CounterInputComponent, ReactiveInputComponent, PasswordInputComponent],
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
-  exports: [GenericInputComponent, CounterInputComponent, ReactiveInputComponent],
+  exports: [GenericInputComponent, CounterInputComponent, ReactiveInputComponent, PasswordInputComponent],
 })
 export class SharedModule {}
