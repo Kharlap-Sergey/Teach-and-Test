f7c866a61154d34d8e4d755e69e8ebe1e5042c07
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-03-12 11:27:00 +0300

    Change navbar scroll behavior

--- TeachAndTest-client/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ TeachAndTest-client/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -11,7 +11,8 @@ import {
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent implements OnInit, AfterViewInit{
+export class NavBarComponent
+  implements OnInit, AfterViewInit {
   @ViewChild('navbar') navBar: any;
   previousY: number;
 
@@ -24,15 +25,19 @@ export class NavBarComponent implements OnInit, AfterViewInit{
 
   @HostListener('window:scroll', ['$event'])
   onScroll($event: Event) {
-    if(this.previousY){
+    if (this.previousY) {
       let opacity = 1;
-      let deltaH = Math.abs(this.previousY - window.pageYOffset)
-      if(deltaH < 50)return;
-      if(this.previousY < window.pageYOffset ){
+      let z = 0;
+      let deltaH = Math.abs(
+        this.previousY - window.pageYOffset
+      );
+      if (deltaH < 50) return;
+      if (this.previousY < window.pageYOffset) {
         opacity = 0;
+        z = -1;
       }
-      this.navBar.nativeElement.style.opacity =
-        opacity ;
+      this.navBar.nativeElement.style.opacity = opacity;
+      this.navBar.nativeElement.style.zIndex = z;
     }
     this.previousY = window.pageYOffset;
   }
