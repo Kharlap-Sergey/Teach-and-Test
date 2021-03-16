060805a30c3a925eab940d06d20ed8c101617fef
snovis <sergeygoodgood@gmail.com>
2021-03-16 09:43:19 +0300

    Add toastr styles averriding ability

--- TeachAndTest-client/src/app/shared/services/notification.service.ts
+++ TeachAndTest-client/src/app/shared/services/notification.service.ts
@@ -2,25 +2,32 @@ import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
-  constructor(private toastr: ToastrService) {
+  constructor(private toastr: ToastrService) {}
+
+  showSuccess(message: string, title: string) {
+    this.toastr.success(message, title, {
+      toastClass: 'toastr',
+    });
   }
 
-  showSuccess(message: string, title:string){
-      this.toastr.success(message, title)
+  showError(message: string, title: string) {
+    this.toastr.error(message, title, {
+      toastClass: 'toastr',
+    });
   }
 
-  showError(message: string, title:string){
-      this.toastr.error(message, title)
+  showInfo(message: string, title: string) {
+    this.toastr.info(message, title, {
+      toastClass: 'toastr',
+    });
   }
 
-  showInfo(message: string, title:string){
-      this.toastr.info(message, title)
-  }
-
-  showWarning(message: string, title:string){
-      this.toastr.warning(message, title)
+  showWarning(message: string, title: string) {
+    this.toastr.warning(message, title, {
+      toastClass: 'toastr',
+    });
   }
 }
