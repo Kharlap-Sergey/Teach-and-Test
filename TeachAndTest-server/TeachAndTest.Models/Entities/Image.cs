7cb08202d359a00caabc2b10863cdac22c7a8abf
snovis <sergeygoodgood@gmail.com>
2021-03-20 15:09:22 +0300

    Add image entities

---
+++ TeachAndTest-server/TeachAndTest.Models/Entities/Image.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeachAndTest.Models.Entities
+{
+    public class Image : Entity<int>
+    {
+        public string Path { get; set; }
+    }
+}
