f00f1c70eb48deedb21785b8b03ecd3a39cf6e90
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-03-30 19:43:40 +0300

    Save

---
+++ TeachAndTest-server/TeachAndTest.Api/Controllers/CourseController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeachAndTest.Api.Common.Controllers;
+
+namespace TeachAndTest.Api.Controllers
+{
+    public class CourseController : ApiControllerBase
+    {
+
+    }
+}
