b17501a2b7c67a8d3dff7e22b29bb517dc3bafd4
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-04-02 15:37:41 +0300

    Fix problem with invalid error occured on the problem

--- TeachAndTest-server/TeachAndTest.Models/Entities/FileDetails.cs
+++ TeachAndTest-server/TeachAndTest.Models/Entities/FileDetails.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using TeachAndTest.Models.Entities.General;
 
 namespace TeachAndTest.Models.Entities
 {
-    public class FileDetails : Entity<Guid>
+    public class FileDetails : Entity<Guid?>
     {
+        [Required]
         public DateTime? Created { get; set; }
         public bool? Deleted { get; set; }
+        [Required]
         public bool IsPrivate { get; set; }
         public string DocumentName { get; set; }
         public string DocType { get; set; }
         public string DocUrl { get; set; }
+        [Required]
         public int? AuthorId { set; get; }
         public User Author { set; get; }
     }
--- TeachAndTest-server/TeachAndTest.Models/Entities/User.cs
+++ TeachAndTest-server/TeachAndTest.Models/Entities/User.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TeachAndTest.Models.Entities
 {
     public class User : IdentityUser<int>
     {
+        [Required]
         public string Firstname { get; set; }
+        [Required]
         public string Lastname { get; set; }
         public Guid? AvatarId { set; get; }
         public FileDetails Avatar { set; get; }
