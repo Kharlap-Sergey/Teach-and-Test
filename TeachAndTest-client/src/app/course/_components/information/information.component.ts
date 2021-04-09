609fc17db2566d02181a895dc9d04bd403af4bc8
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-04-09 10:34:34 +0300

    Add some frames to inforamtion fragment

--- TeachAndTest-client/src/app/course/_components/information/information.component.ts
+++ TeachAndTest-client/src/app/course/_components/information/information.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { CourseModel } from '@app/course/_models/course.model';
+import { CourseService } from '@app/course/_services/course.service';
 
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
-  styleUrls: ['./information.component.scss']
+  styleUrls: ['./information.component.scss'],
 })
 export class InformationComponent implements OnInit {
+  private courseId: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public course = new CourseModel();
+  
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private courseService: CourseService
+  ) {
+    this.courseId = this.activateRoute.snapshot.params["id"];
   }
 
+  ngOnInit(): void {
+    this.courseService.getCourseDetails(this.courseId)
+        .subscribe(
+          (course: CourseModel) => {
+            this.course = course;
+          }
+        )
+  }
 }
