2e529b13de4628e46f36f3cf2f8293eeeb47ee3d
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-04-16 15:47:38 +0300

    Stylize history tools

--- TeachAndTest-client/src/app/rich-text-editor/rich-text-editor.component.ts
+++ TeachAndTest-client/src/app/rich-text-editor/rich-text-editor.component.ts
@@ -1,17 +1,53 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-rich-text-editor',
   templateUrl: './rich-text-editor.component.html',
   styleUrls: ['./rich-text-editor.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RichTextEditorComponent implements OnInit {
+export class RichTextEditorComponent
+  implements OnInit {
+  public controls = {
+    history: [
+      {
+        imageSrc: 'rich-text-editor/back.png',
+        active: false,
+        click: () => {},
+        capture: 'go back',
+      },
+      {
+        imageSrc: 'rich-text-editor/forward.png',
+        active: false,
+        click: () => {},
+        capture: 'go forward',
+      },
+    ],
+    styles: {
+      bold: {
+        value: false,
+        active: true,
+        capture: '',
+      },
+      italic: {
+        value: false,
+        active: true,
+        capture: '',
+      },
+      underline: {
+        value: false,
+        active: true,
+        capture: '',
+      },
+    },
+  };
   public value = false;
-  
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
