afb6ef0ab41f8b1c6bb5cd92e8077eb97f1fdbe0
Siarhei Kharlap <sergeygoodgood@gmail.com>
2021-04-23 19:44:28 +0300

    Save

--- TeachAndTest-client/src/app/rich-text-editor/_components/html-editor/html-editor.component.ts
+++ TeachAndTest-client/src/app/rich-text-editor/_components/html-editor/html-editor.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-html-editor',
@@ -7,7 +11,31 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ['./html-editor.component.scss'],
 })
 export class HtmlEditorComponent implements OnInit {
-  ngOnInit(): void {
-  }
+  @ViewChild('fakeInput')
+  private fakeInput: ElementRef;
+  @ViewChild('htmlEditor')
+  private htmlEditor: ElementRef;
 
+  public isEditorUnderFocus: boolean = false;
+
+  ngOnInit(): void {}
+
+  public handleEditorFocus(event: any) {
+    if(this.isEditorUnderFocus){
+      event.preventDefault();
+      return;
+    }
+    this.isEditorUnderFocus = true;
+
+    this.fakeInput.nativeElement.focus();
+    //console.log(event);
+  }
+  public handleEditorBlur(event: any) {
+    if(event.relatedTarget == this.htmlEditor.nativeElement) {
+      event.preventDefault();
+      return;
+    }
+    this.isEditorUnderFocus = false;
+    console.log(event);
+  }
 }
